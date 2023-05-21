4ce509f29e6bbac32f9d88c9e9e88df2869bde2e
Mrinmoy Pal <mrinmoy21pal@gmail.com>
2023-05-22 01:22:45 +0530

    First Snapshot written successfully

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,8 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer' //for Jest Snapshot
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+test('App snapshot test', ()=>{
+  const component = renderer.create(<App />);
+  const tree  = component.toJSON();
+  expect(tree).toMatchSnapshot();
 });
+
+// it('renders without crashing', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<App />, div);
+// });
