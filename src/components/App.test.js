dca3b11a359f9a398d4f12d0435fec743286a8ca
Mrinmoy Pal <mrinmoy21pal@gmail.com>
2024-01-17 23:58:42 +0530

    change

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -4,7 +4,7 @@ import App from './App';
 
 test('App snapshot test', ()=>{
   const component = renderer.create(<App />);
-  const tree  = component.toJSON();
+  const tree = component.toJSON();
   expect(tree).toMatchSnapshot();
 }); 
 
